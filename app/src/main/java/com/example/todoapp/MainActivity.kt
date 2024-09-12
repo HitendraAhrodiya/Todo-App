1b47b86fedbb3ae8afbdb2d1063550885d9397a6
hitendra <ahrodiyahitendra@gmail.com>
2024-09-12 21:22:06 +0530

    first commit

---
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.todoapp
+
+import android.content.ContentValues.TAG
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.ViewModelProvider
+import com.example.todoapp.ui.theme.TodoAppTheme
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+
+import java.text.SimpleDateFormat
+
+class MainActivity : ComponentActivity() {
+    private val personCollectionRef = Firebase.firestore.collection("person")
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val todoViewModel = ViewModelProvider(this)[TodoViewModel::class.java]
+        enableEdgeToEdge()
+        setContent {
+            TodoAppTheme {
+                TodoListPage(todoViewModel)
+            }
+        }
+    }
+
+    private fun savenotes(person: Todo) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            personCollectionRef.add(person).await()
+            withContext(Dispatchers.Main){
+                Toast.makeText(this@MainActivity,"Successful add notes", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
+
+
+
+
---
+++ app/src/main/java/com/example/todoapp/Note.kt
@@ -0,0 +1,7 @@
+package com.example.todoapp
+
+import java.util.Date
+
+
+data class Note(val content: String, val createdAt: Date)
+
